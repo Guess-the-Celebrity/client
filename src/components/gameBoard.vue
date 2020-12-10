faf093060f8b81be09c2f5d24b89ac6f57b85a34
MozartMongi <saya.mozart@gmail.com>
2020-12-10 23:15:43 +0700

    add success point for player create player card component add sweet alert cdn in index.html

--- src/components/gameBoard.vue
+++ src/components/gameBoard.vue
@@ -10,12 +10,7 @@
                       </div>
                   </div>
                   <ul class="list-unstyled">
-                    <li class="media">
-                        <img src="../img/star.png" class="img-player ml-2" alt="...">
-                        <div class="media-body text-left ml-3 mt-2">
-                        <h5 class="mt-0 mb-1 text-white font-weight-bold text-monospace">Mozart</h5>
-                        </div>
-                    </li>
+                    <PlayerCard></PlayerCard>
                   </ul>
               </div>
           </div>
@@ -38,11 +33,15 @@
 </template>
 
 <script>
+import PlayerCard from '../components/playerCard'
 export default {
   data () {
     return {
       answer: ''
     }
+  },
+  components: {
+    PlayerCard
   },
   methods: {
     submitAnswer () {
